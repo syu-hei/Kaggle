d6b127afc92a66b2be5a49a8f2f4d297346802c7
syu-hei <53079859+syu-hei@users.noreply.github.com>
2020-08-06 14:46:33 +0900

    first commit

---
+++ threshold(閾値).py
@@ -0,0 +1,20 @@
+# 閾値を80%に設定
+def msv1(data, thresh=20, color='black', edgecolor='black', width=15, height=3):
+    """
+    SOURCE: https://www.kaggle.com/amiiiney/price-prediction-regularization-stacking
+    """
+    
+    plt.figure(figsize=(width,height))
+    percentage=(data.isnull().mean())*100
+    percentage.sort_values(ascending=False).plot.bar(color=color, edgecolor=edgecolor)
+    plt.axhline(y=thresh, color='r', linestyle='-')
+    plt.title('Missing values percentage per column', fontsize=20, weight='bold' )
+    plt.text(len(data.isnull().sum()/len(data))/1.7, thresh+12.5, f'Columns with more than {thresh}% missing values', fontsize=12, color='crimson',
+         ha='left' ,va='top')
+    plt.text(len(data.isnull().sum()/len(data))/1.7, thresh - 5, f'Columns with less than {thresh} missing values', fontsize=12, color='green',
+         ha='left' ,va='top')
+    plt.xlabel('Columns', size=15, weight='bold')
+    plt.ylabel('Missing values percentage')
+    plt.yticks(weight ='bold')
+    
+    return plt.show()
