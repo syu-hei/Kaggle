80ece0e08b58ce88a748ee59e93b6ce0481858af
syu-hei <53079859+syu-hei@users.noreply.github.com>
2020-08-21 00:55:52 +0900

    kaggle

---
+++ graph/pairs_plot.py
@@ -0,0 +1,32 @@
+# Copy the data for plotting
+plot_data = ext_data.drop(columns = ['DAYS_BIRTH']).copy()
+
+# Add in the age of the client in years
+plot_data['YEARS_BIRTH'] = age_data['YEARS_BIRTH']
+
+# Drop na values and limit to first 100000 rows
+plot_data = plot_data.dropna().loc[:100000, :]
+
+# Function to calculate correlation coefficient between two columns
+def corr_func(x, y, **kwargs):
+    r = np.corrcoef(x, y)[0][1]
+    ax = plt.gca()
+    ax.annotate("r = {:.2f}".format(r),
+                xy=(.2, .8), xycoords=ax.transAxes,
+                size = 20)
+
+# Create the pairgrid object
+grid = sns.PairGrid(data = plot_data, size = 3, diag_sharey=False,
+                    hue = 'TARGET', 
+                    vars = [x for x in list(plot_data.columns) if x != 'TARGET'])
+
+# Upper is a scatter plot
+grid.map_upper(plt.scatter, alpha = 0.2)
+
+# Diagonal is a histogram
+grid.map_diag(sns.kdeplot)
+
+# Bottom is density plot
+grid.map_lower(sns.kdeplot, cmap = plt.cm.OrRd_r);
+
+plt.suptitle('Ext Source and Age Features Pairs Plot', size = 32, y = 1.05);
