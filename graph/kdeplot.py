62cf70c19071de9e2eb4e1cb758bb9281d6580f6
syu-hei <53079859+syu-hei@users.noreply.github.com>
2020-08-20 21:03:51 +0900

    kaggle

---
+++ graph/kdeplot.py
@@ -0,0 +1,10 @@
+plt.figure(figsize = (10, 8))
+
+# KDE plot of loans that were repaid on time
+sns.kdeplot(app_train.loc[app_train['TARGET'] == 0, 'DAYS_BIRTH'] / 365, label = 'target == 0')
+
+# KDE plot of loans which were not repaid on time
+sns.kdeplot(app_train.loc[app_train['TARGET'] == 1, 'DAYS_BIRTH'] / 365, label = 'target == 1')
+
+# Labeling of plot
+plt.xlabel('Age (years)'); plt.ylabel('Density'); plt.title('Distribution of Ages');
